78e101e9917b3fbc54f5b522c98c9bcb591f1cfc
Chaewon Kang <saturday.the17th@gmail.com>
2021-09-27 14:48:39 +0900

    HotFix

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import '../static/css/style.css';
+import ogImage from '../static/images/ogImage.png';
 
 function MyApp({ Component, pageProps }) {
+  const currentUrl = 'https://adj.kr';
   return (
     <>
       <Head>
@@ -10,6 +12,13 @@ function MyApp({ Component, pageProps }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <meta property='og:url' content={currentUrl} />
+        <meta property='og:title' content='adj' />
+        <meta
+          property='og:description'
+          content='Adj is a creative agency specializing in branding and advertising.'
+        />
+        <meta property='og:image' content={ogImage} />
         <title>ADJ</title>
       </Head>
       <Component {...pageProps} />
